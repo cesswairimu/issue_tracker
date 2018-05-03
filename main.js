7d87d3e6ec01424c95b6e2ae1602cd0e456a4a69
Cess <cessmbuguar@gmail.com>
2018-05-03 12:18:09 +0300

    set closed status

--- main.js
+++ main.js
@@ -48,3 +48,15 @@ function saveIssue(e) {
     fetchIssue();
     e.preventDefault();
 }
+
+function setStatusClosed (id) {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+
+    for (var i = 0; i < issues.length; i++){
+        if (issues[i].id == id) {
+            issues[i].status = 'Closed';
+        }
+    }
+    localStorage.setItem('issues', JSON.stringify('issues'));
+    fetchIssues();
+}
