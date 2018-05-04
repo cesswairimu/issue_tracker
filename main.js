89d5bd1ccd93efd64672553ef487f4fc7cd2b483
Cess <cessmbuguar@gmail.com>
2018-05-04 12:25:20 +0300

    delete function for issue

--- main.js
+++ main.js
@@ -60,3 +60,15 @@ function setStatusClosed (id) {
     localStorage.setItem('issues', JSON.stringify('issues'));
     fetchIssues();
 }
+
+function deleteIssue (id) {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+
+    for (var i=0; i < issues.length; i++ ){
+        if (issues[i].id == id){
+            issues.splice(i, 1);
+        }
+    }
+    localStorage.setItem('issues', JSON.stringify('issues'));
+    fetchIssues();
+}
